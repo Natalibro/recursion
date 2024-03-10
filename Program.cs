a137322998a9a3935cfcf3a7b5d054669c5bb2da
Natalibro <brodka@mail.ru>
2024-03-11 01:11:08 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+static void Nature (int m, int n)
+{
+    if (m > 0 && m <= n)
+    {
+        Console.WriteLine(m);
+    }
+    else if (m > 0 && m >= n)
+    {
+        Console.WriteLine(m);
+    }
+    if (m < n)
+    {
+        Nature(m + 1, n);
+    }
+    else if (m > n)
+    {
+        Nature(m - 1, n);
+    }
+}
+Nature(2, 8);
